71611a89b93571a26006b50d0ed9530446a71c7c
Darwin S <darpart19@gmail.com>
2022-08-24 15:13:07 +0530

    pushing theme flolders

---
+++ assets/js/darwinconsulting.js
@@ -0,0 +1,59 @@
+$('.our-services-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+$('.our-team-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+$('.our-testimonial-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+jQuery(document).ready(function() {
+    jQuery('.toggle-nav').click(function(e) {
+        jQuery('.menu.main ul').slideToggle(500);
+  
+        e.preventDefault();
+    });
+     
+});
---
+++ template-parts/home/template-home-1.php
@@ -0,0 +1,310 @@
+<?php
+/*
+Template Name: Home 1
+*/
+?>
+<section id="home-banner-section" class="banner">
+    <div class="container">
+        <div class="row">
+            <p class="home-banner-highlight">REPEAT PREDEFINED</p>
+            <h3 class="home-banner-title">Developing Your Opportunities<br>Within Your Business</h3>
+            <p class="home-banner-subtitle">Many variations of passages of torem spsum available, but the majority have suffered afteration in same form, by injected humour, randomised...</p>
+            <div class="home-banner-btn-wrapper">
+                <a href="#" class="theme-button">VIEW MORE</a>
+            </div>
+        </div>
+  </div>
+</section>
+
+<section id="home-services-section">
+        <div class="row">
+            <div class="col-md-3 home-our-services-left">
+                <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/what_we-1.png" />
+            </div>
+            <div class="col-md-9 home-our-services-right">
+            <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/what_we_bg.jpg" />
+            </div>
+        </div>
+</section>
+
+<section id="home-our-work-section" class="home-our-work-section carousel-section">
+    <div class="container">
+        <div class="row">
+            <h1 class="consulting-side-heading">OUR WORK</h1>
+            <h1 class="consulting-main-heading">Finished Projects</h1>
+            <p>fsdsgfhhj</p>
+            <div class="owl-carousel our-services-carousel">
+                <div class="item">
+                    <div class="carousel-item-image projects-carousel-image">
+                        <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/project-1.jpg" />
+                    </div>
+                    <div class="carousel-item-content team-carousel-content">
+                        <h3 class="carousel-item-title">Uses a dictionary</h3>
+                        <p class="carousel-item-subtitle">Look words which</p>
+                        <p class="carousel-item-paragraph">dvxfdfhdhh</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="carousel-item-image projects-carousel-image">
+                        <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/project-1.jpg" />
+                    </div>
+                    <div class="carousel-item-content team-carousel-content">
+                        <h3 class="carousel-item-title">Randomised Words</h3>
+                        <p class="carousel-item-subtitle">Words which don</p>
+                        <p class="carousel-item-paragraph">dvxfdfhdhh</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="carousel-item-image projects-carousel-image">
+                        <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/project-1.jpg" />
+                    </div>
+                    <div class="carousel-item-content team-carousel-content">
+                        <h3 class="carousel-item-title">Suffered Alteration</h3>
+                        <p class="carousel-item-subtitle">Don't look words which</p>
+                        <p class="carousel-item-paragraph">dvxfdfhdhh</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="carousel-item-image projects-carousel-image">
+                        <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/project-1.jpg" />
+                    </div>
+                    <div class="carousel-item-content team-carousel-content">
+                        <h3 class="carousel-item-title">Uses a dictionary</h3>
+                        <p class="carousel-item-subtitle">Look words which</p>
+                        <p class="carousel-item-paragraph">dvxfdfhdhh</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="home-humour-section" style="background: url('https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/humour.jpg')">
+    <div class="container">
+        <div class="row">
+            <h1 class="consulting-side-heading">OUR WORK</h1>
+            <h1 class="consulting-main-heading">Some form by humour</h1>
+            <p>
+                passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn’t anything embarrassing hidden in the middle of text.
+            </p>
+        </div>
+    </div>
+</section>
+
+<section id="our-work-bottom-section">
+<div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Vision</h3>
+                <p>passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+            </div>
+            <div class="col-md-6">
+                <h3>Mission</h3>
+                <p>passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="home-our-team-section" class="home-our-team-section carousel-section">
+    <div class="container">
+        <div class="row">
+            <h1 class="consulting-side-heading">OUR TEAM</h1>
+            <h1 class="consulting-main-heading heading-blue">Excellent Expert Advisors</h1>
+            <p>Passage of lorum ipsum available Passage of lorum ipsum available Passage of lorum ipsum availablePassage of lorum ipsum available Passage of lorum ipsum availablePassage of lorum ipsum availablePassage of lorum ipsum available</p>
+            <div class="owl-carousel our-team-carousel">
+                <div class="item">
+                    <div class="carousel-item-image team-carousel-image">
+                        <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/team-1.jpg" />
+                    </div>
+                    <div class="carousel-item-content team-carousel-content">
+                        <h3 class="carousel-item-title">Dr.Danial</h3>
+                        <p class="carousel-item-subtitle">Slightly believable</p>
+                        <p class="carousel-item-paragraph">dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="carousel-item-image team-carousel-image">
+                        <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/team-2.jpg" />
+                        <div class="carousel-overlay team-carousel-social">
+                            <ul class="carousel-overlay-social-list">
+                                <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
+                                <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
+                                <li><i class="fa fa-google-plus" aria-hidden="true"></i></li>
+                                <li><i class="fa fa-linkedin-square" aria-hidden="true"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="carousel-item-content team-carousel-content">
+                        <h3 class="carousel-item-title">Dan O'Brien</h3>
+                        <p class="carousel-item-subtitle">words which don't look</p>
+                        <p class="carousel-item-paragraph">dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="carousel-item-image team-carousel-image">
+                        <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/team-3.jpg" />
+                    </div>
+                    <div class="carousel-item-content team-carousel-content">
+                        <h3 class="carousel-item-title">Amanda Seyfriend</h3>
+                        <p class="carousel-item-subtitle">Mandemised Words</p>
+                        <p class="carousel-item-paragraph">dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="carousel-item-image team-carousel-image">
+                        <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/team-2.jpg" />
+                    </div>
+                    <div class="carousel-item-content team-carousel-content">
+                        <h3 class="carousel-item-title">Dan O'Brien</h3>
+                        <p class="carousel-item-subtitle">words which don't look</p>
+                        <p class="carousel-item-paragraph">dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh dvxfdfhdhh</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="home-achieve-section" style="background: url('https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/plan.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 left-column">
+                <h3>Achieve your Business Plan...</h3>
+                <p>Passages of loum ipsum available, form, by injected humour...</p>
+            </div>
+            <div class="col-md-4 right-column">
+            <div class="home-banner-btn-wrapper">
+                <a href="#" class="theme-button">GET A QUOTE</a>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="home-testimonial-section" class="home-testimonial-section carousel-section">
+    <div class="container">
+        <div class="row">
+            <h1 class="consulting-side-heading">TESTIMONIAL</h1>
+            <h1 class="consulting-main-heading heading-blue">Happy Clients</h1>
+        </div>
+        <div class="container testimonial-contents-wrapper">
+            <div class="row">
+                <div class="owl-carousel our-testimonial-carousel">
+                <?php
+                /*
+                $testimonial_query = new WP_Query(
+                    
+                    array(
+                        
+                        'post_type' => 'testimonials',
+                        'post_status' => 'publish',
+                        'post_per_page' => -1
+                ));
+
+                while( $testimonial_query->have_posts() ) {
+                    
+                    $testimonial_query->the_post();
+                    $testimonial_id = get_the_ID();
+                    $testimonial_name = get_the_title();
+                    $testimonial_designation = get_the_expert();
+                    $testimonial = get_the_content();
+                    
+                    if( $testimonial ) {
+                    */
+                    ?>
+                        <div class="row item">
+                        <div class="col-md-4 testimonial-image">
+                            <img src="https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/testi-1.png">
+                        </div>
+                        <div class="col-md-8 testimonial-contents">
+                            <p class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <h3 class="carousel-item-title">Dan O'Brien</h3>
+                            <p class="carousel-item-subtitle">CEO Founder</p>
+                        </div>
+                    </div>
+                    <?php
+                    /*
+                    }
+                }
+
+                wp_reset_query();
+                */
+                ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="home-counter-section" style="background: url('https://demo.darwinssv.com/multidots/wp-content/uploads/2022/08/counter_bg.png')">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 counter-item">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <h3>100%</h3>
+                <p>Satisfied Customers</p>
+            </div>
+            <div class="col-md-3 counter-item">
+                <i class="fa fa-square" aria-hidden="true"></i>
+                <h3>100%</h3>
+                <p>Projects Completed</p>
+            </div>
+            <div class="col-md-3 counter-item">
+                <i class="fa fa-clock" aria-hidden="true"></i>
+                <h3>100%</h3>
+                <p>Hours Worked</p>
+            </div>
+            <div class="col-md-3 counter-item">
+                <i class="fa fa-file-text" aria-hidden="true"></i>
+                <h3>100%</h3>
+                <p>Awards Winning</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="home-contact-section" class="home-contact-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 left-column">
+                <h1 class="consulting-side-heading">CONTACT US</h1>
+                <h1 class="consulting-main-heading">Lets get in touch</h1>
+                <div class="contact-form-container">
+                    <form id="contact-form">
+                        <div class="form-group">
+                            <input type="text" name="name" placeholder="Name" required />
+                            <input type="mail" name="main" placeholder="Email" required />
+                        </div>
+                        <div class="form-group">
+                            <input type="tel" name="phone" placeholder="Phone Number" required />
+                            <input type="text" name="subject" placeholder="Subject" required />
+                        </div>
+                        <textarea placeholder="How Can I Help?"></textarea>
+                        <input type="submit" name="submit" value="SUBMIT" />
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-6 right-column">
+                <div class="address-item">
+                    <h3>Address<h3>
+                    <p><a href="#">5370 S Arrowhead Trl, Baldwin, MI, 49304</a></p>
+                </div>
+                <div class="address-item">
+                    <h3>Email<h3>
+                    <p><a href="mailto:artistpoint.com">artistpoint.com</a></p>
+                </div>
+                <div class="address-item">
+                    <h3>Phone number<h3>
+                    <p><a href="tel:+911234567098">+911234567098</a></p>
+                </div>                
+            </div>
+        </div>
+    </div>
+</section>
